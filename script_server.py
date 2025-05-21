d06b1d5f26df741bc359da755d2819260983755f
Rumpel stilskin <rozbujnik@gmail.com>
2025-05-21 22:29:45 +0200

    Fixed error

--- script_server.py
+++ script_server.py
@@ -27,7 +27,7 @@ from src import CrawlHistory
 # increment major version digit for releases, or link name changes
 # increment minor version digit for JSON data changes
 # increment last digit for small changes
-__version__ = "3.0.41"
+__version__ = "3.0.42"
 
 
 app = Flask(__name__)
