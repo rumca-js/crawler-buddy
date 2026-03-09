ed18cb8fd90057b4ab08cf1dbb04c467470e9bdc
Rumpel Stilskin <renegat@renegat0x0.ddns.net>
2026-03-09 18:27:16 +0100

    Added watchdog script

---
+++ watchdog.py
@@ -0,0 +1,24 @@
+
+import subprocess
+import time
+
+
+command = ["poetry", "run", "python", "script_server.py", "-k", "-m"]
+
+
+def run_forever():
+    while True:
+        try:
+            print("Starting script")
+            process = subprocess.Popen(command)
+            return_code = process.wait()
+            print(f"Process exited with code {return_code}. Restarting...")
+        except KeyboardInterrupt:
+            print("Runner stopped by user.")
+            return
+        except Exception as E:
+            print(f"Error {e}. Restarting...")
+
+
+if __name__ == "__main__":
+    run_forever()
